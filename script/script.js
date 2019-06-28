47a789f93522104b7e526c2c8ee2ebeb76b1fac6
Iankimble <38480036+Iankimble@users.noreply.github.com>
2019-06-28 07:50:11 -0400

    Update script.js

--- script/script.js
+++ script/script.js
@@ -107,7 +107,9 @@ $("document").ready(() => {
         window.open("https://iankimble.github.io/yahrnPrototype/");
     });
 
-
+    $("#nbaCard").click(()=>{
+    window.open("https://www.behance.net/gallery/82201761/NBA-trading-card-concepts?")
+    });
 
 
 });
